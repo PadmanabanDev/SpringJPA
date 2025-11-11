74f9cbc213dccd9309e7222ab0ea64268cd18027
naban <padmanabhan1997@gmail.com>
2025-11-11 08:21:26 +0530

    entities added

--- src/main/java/com/pn/springjpa/SpringJpaApplication.java
+++ src/main/java/com/pn/springjpa/SpringJpaApplication.java
@@ -1,5 +1,8 @@
 package com.pn.springjpa;
 
+import com.pn.springjpa.entities.Address;
+import com.pn.springjpa.entities.Profile;
+import com.pn.springjpa.entities.Tag;
 import com.pn.springjpa.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +11,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJpaApplication {
 
     public static void main(String[] args) {
-     //   SpringApplication.run(SpringJpaApplication.class, args);
+       // SpringApplication.run(SpringJpaApplication.class, args);
 
        // User user = new User(1L, "Padmanaban","naban@gmail.com","7113pn");
 
         User user=User.builder()
-                .name("padmanaban")
+                .name("Padmanaban")
                 .email("naban@gmail.com")
                 .password("7113pn")
                 .build();
+//
+////        Address address = Address.builder()
+////                .street("street")
+////                .city("city")
+////                .state("state")
+////                .zip("zip")
+////                .build();
+////
+////        user.addAddress(address);
+//
+       //user.addTag("tag1");
+        var profile = Profile.builder()
+                        .bio("bio")
+                                .build();
+//
+//
+user.setProfile(profile);
 
+profile.setUser(user);
+        System.out.println(user);
     }
+
+
 
 }
